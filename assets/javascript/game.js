272feba1678b87ecca3f3be59476fb3ee2cf120b
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-09 18:40:58 -0700

    Added 6 questions plus another variable question seven for restarting the quiz

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,3 +16,65 @@ var quizNum = 0;
 var correctAnswers = 0;
 var intervalId;
 
+var questionOne =
+{
+    question: $("#question").html("What is the name of the Sensei that taught Naruto how to use 'Rasengan'?"),
+    choice1:$("#choice").html("Kakashi"),
+    choice2:$("#answer").html("Jiraya"),
+    choice3:$("#choice").html("Iruka"),
+    choice4:$("#choice").html("Minato"),
+}
+
+var questionTwo =
+{
+    question: $("#question").html("Who did Naruto end up dating?"),
+    choice1:$("#choice").html("Tsunade"),
+    choice2:$("#choice").html("Sakura"),
+    choice3:$("#choice").html("Tenten"),
+    choice4:$("#answer").html("Hinata"),
+}
+
+var questionThree = 
+{
+    question: $("#question").html("What is the name of the character that eats alot to enhance his powers?"),
+    choice1:$("#answer").html("Choji"),
+    choice2:$("#choice").html("Naruto"),
+    choice3:$("#choice").html("Itachi"),
+    choice4:$("#choice").html("Boruto"),
+}
+
+var questionFour = 
+{
+    question: $("#question").html("Which character resembles the same features as the famous real-life martial artist, 'Bruce Lee'?"),
+    choice1:$("#choice").html("Guy"),
+    choice2:$("#choice").html("Sasuke"),
+    choice3:$("#answer").html("Rock"),
+    choice4:$("#choice").html("Gaara"),
+}
+
+var questionFive = 
+{
+    question: $("#question").html("What is the name of the Nine-Tailed Fox that is sealed in Naruto's body?"),
+    choice1:$("#choice").html("Shukaku"),
+    choice2:$("#answer").html("Kurama"),
+    choice3:$("#choice").html("Isobu"),
+    choice4:$("#choice").html("Matatabi"),
+}
+
+var questionSix =
+{
+    question: $("#question").html("In what year was the Naruto Manga published?"),
+    choice1:$("#choice").html("1994"),
+    choice2:$("#answer").html("1997"),
+    choice3:$("#choice").html("2001"),
+    choice4:$("#choice").html("2007"),
+}
+
+var questionSeven =
+{
+    question:$("#restart").html("Restart Quiz?"),
+    choice1:'',
+    choice2:'',
+    choice3:'',
+    choice4:'',
+}
