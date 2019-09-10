8a37da2f497b84f5b92a64634791bdac236a7115
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-09 18:22:43 -0700

    Added list of instructions step by step for game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Make a variable that keeps the time count with intervalId, correctAnswers, current number page.
+//Make a variable of each questions that includes questions and answer choices.
+//Create a time converter for seconds.
+//Create a time count down with setIntervals.
+//Create a time count down with a statement if the time reaches "0" that will move on to the next question.
+//Create a function that displays the questions.
+//Create a function that will automatically display the next question.
+//Create a function that will increment correct answers by 1 and shows a image that you are correct.
+//Hint: Make sure to use clearInterval and setTimeout to go to next question.
+//Create a function that will show wrong answer with image and move on to the next question.
+//Create a function that will reset the game.
+//Create a start button function that will execute the game.
